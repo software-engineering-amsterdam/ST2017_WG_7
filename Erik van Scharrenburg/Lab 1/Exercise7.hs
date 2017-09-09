7f762b53ff89107876ce4649ddd171ecb0d56885
evanscharrenburg <evanscharrenburg@users.noreply.github.com>
2017-09-09 20:27:13 +0200

    Lab 1 Exercise 7 - Added type declarations
    Changed digitToInt to digitToInteger as the Int could overflow in undigits function with large numbers.

--- Erik van Scharrenburg/Lab 1/Exercise7.hs
+++ Erik van Scharrenburg/Lab 1/Exercise7.hs
@@ -1,5 +1,4 @@
 module Lab1 where
-import Data.Char
 import Data.List
 import Test.QuickCheck
 
@@ -14,34 +13,47 @@ luhn n = mod (sum (doubleEverySecond n)) 10 == 0
           where subNine n | n > 9 = n - 9
                           | otherwise = n
 
+isAmericanExpress :: Integer -> Bool
 isAmericanExpress n = luhn n
                    && length (digits n) == 15
                    && checkIINs n [34,37]
 
+isMaster :: Integer -> Bool
 isMaster n = luhn n
           && length (digits n) == 16
           && checkIINs n [51..55]
           && checkIINs n [2221..2720]
 
+isVisa :: Integer -> Bool
 isVisa n = luhn n
         && ( length (digits n) == 13
           || length (digits n) == 16
           || length (digits n) == 19)
         && checkIIN n 4
 
+checkIINs :: Integer -> [Integer] -> Bool
 checkIINs n = any (checkIIN n)
+
+checkIIN :: Integer -> Integer -> Bool
 checkIIN n iinToCheck = iin == iinToCheck
   where iin = undigits (take (length (digits iinToCheck)) (digits n))
 
-digits n = map digitToInt (show n)
+digits :: Integer -> [Integer]
+digits n = map digitToInteger (show n)
+  where digitToInteger c = read [c] :: Integer
+
+undigits :: [Integer] -> Integer
 undigits = foldl1 (\a b -> 10 * a + b)
 
+myLuhnTest :: Integer -> Bool
 myLuhnTest n = luhn n == snd (head (filter (\x -> fst x == n) luhnTestList))
   where myLuhnTest' = forAll luhnTestListGen myLuhnTest
 
+luhnTestListGen :: Gen Integer
 luhnTestListGen = elements (map fst luhnTestList)
 
 -- Format: (number, Boolean indicating whether number is valid)
+luhnTestList :: [(Integer, Bool)]
 luhnTestList = [(1, False),
                 (18, True),
                 (1234567890, False),
@@ -54,5 +66,3 @@ luhnTestList = [(1, False),
                 (936502673, True)]
 
 -- Time: 3 hours
-
--- (1) Software Testing lecture slides
