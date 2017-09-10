7454ae027e9cbbc3c245da6c00f01d530c8b1de9
Joana Magalhães <Joana Magalhes@192.168.178.108>
2017-09-10 17:34:20 +0200

    Few changes for the final submission

--- Submission Versions/Lab 1/Exercise1.hs
+++ Submission Versions/Lab 1/Exercise1.hs
@@ -11,8 +11,7 @@ calc:: Int -> Int
 calc x = div (x * (x+1) * (2*x + 1)) 6
 
 myTest:: Int -> Bool
-myTest 0 = True
-myTest x = (mySum x) == (calc x)
+myTest (Positive x) = (mySum x) == (calc x)
 
 --For the second quickCheck
 mySum3::Int -> Int
@@ -22,8 +21,7 @@ calc3::Int -> Int
 calc3 x = (div (x * (x + 1)) 2)^2
 
 myTest3::Int -> Bool
-myTest3 0 = True
-myTest3 x = (mySum3 x) == (calc3 x)
+myTest3 (Positive x) = (mySum3 x) == (calc3 x)
 
 --The time spent on this exercise was around 3 hours
 
