b68ccdb3cee88c25f35600e944c7e857e0524784
Joana Magalhães <Joana Magalhes@192.168.178.108>
2017-09-17 09:45:32 +0200

    Assignment 2 exercise 1 fixed

--- Assignment 2/Joana Correia Magalhães Sousa/Lab2_Ex1.hs
+++ Assignment 2/Joana Correia Magalhães Sousa/Lab2_Ex1.hs
@@ -23,16 +23,16 @@ numElemsPerQuart min max xs = length (filter (\x -> ((x >= min) && (x < max))) x
 
 runProbs:: Int -> IO [Int]
 runProbs l = do
-	val <- probs l
-	let val2=numElemsPerQuart 0 0.25 val
+        val <- probs l
+        let val2=numElemsPerQuart 0 0.25 val
             val3=numElemsPerQuart 0.25 0.5 val
             val4=numElemsPerQuart 0.5 0.75 val
             val5=numElemsPerQuart 0.75 1 val
-	return [val2, val3, val4, val5]
+        return [val2, val3, val4, val5]
 
 
 
-check:: [Int] -> Float -> Float -> Bool
+check:: Integral a => [a] -> a -> a -> Bool
 check [] _ _ = True
 check (x:xs) min max  
         |(x >= min) && (x <= max) = check xs min max
@@ -43,14 +43,20 @@ counting l = l * 0.05
 
 testProbs:: Int -> IO Bool
 testProbs l = do
-        let num = (div l 4)
-            dif = fromIntegral(counting l)
+        let num = (fromIntegral l) / 4
+            dif = counting (fromIntegral l)
         xs <- runProbs l
-        return (check xs (num - dif) (num + dif))
+        return (check xs (floor(num - dif)) (floor(num + dif)))
 
 
+--Time spent: 5 hours
+{--
 
+We do the test with 95% certainty, that is, we verify each quartile
+with 5% error. With a length like 10000, we can see that the 
+proportions between each quartile are as expected
 
+--}
 
 
 
