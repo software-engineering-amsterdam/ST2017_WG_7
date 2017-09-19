5fce728c0825a62f32b803f458b884cc9466f960
evanscharrenburg <evanscharrenburg@users.noreply.github.com>
2017-09-19 21:25:40 +0200

    Fixed Lab 3 exercises 3 and 4

--- Assignment 3/Erik van Scharrenburg/Exercise4.hs
+++ Assignment 3/Erik van Scharrenburg/Exercise4.hs
@@ -19,16 +19,20 @@ prop_equiv :: Form -> Form -> Bool
 prop_equiv a b = a `equiv` b
 
 prop_cnf :: Form -> Bool
-prop_cnf (Cnj fs) = all propOrNegProp fs
-prop_cnf (Dsj fs) = all propOrNegProp fs
-prop_cnf f = propOrNegProp f
+prop_cnf (Cnj fs) = all clause fs
+prop_cnf (Dsj fs) = all literal fs
+prop_cnf f = literal f
 
-propOrNegProp :: Form -> Bool
-propOrNegProp (Prop _) = True
-propOrNegProp (Neg (Prop _)) = True
-propOrNegProp _ = False
+clause :: Form -> Bool
+clause (Dsj fs) = all literal fs
+clause f = literal f
+
+literal :: Form -> Bool
+literal (Prop _) = True
+literal (Neg (Prop _)) = True
+literal _ = False
 
 -- 'quickCheck cnfTest' will check that a random formula f is equivalent to
 -- cnf f and that cnf f is in conjunctive normal form.
 
--- Time: 1 hour and 15 minutes
+-- Time: 1 hour and 30 minutes
