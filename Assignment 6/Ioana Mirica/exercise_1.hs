de04ebf54779a571fbead8abca8d407830f14949
Ioana Mirica <ioana.mirica@student.uva.nl>
2017-10-15 18:00:49 +0200

    first ex working

---
+++ Assignment 6/Ioana Mirica/exercise_1.hs
@@ -0,0 +1,9 @@
+module Lab6 where
+
+exM :: Integer -> Integer -> Integer -> Integer
+exM x y n = 
+    if n == 1 then x `rem` n else untilPoint x 1 y n
+
+untilPoint :: Integer -> Integer -> Integer -> Integer -> Integer
+untilPoint x e y n = 
+    if y == 1 then (x*e) `rem` n else untilPoint x ((x*e) `rem` n) (y-1) n
